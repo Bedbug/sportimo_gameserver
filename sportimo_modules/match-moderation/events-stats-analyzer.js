bd2d1fb3ec44ae9f78c9573eeeefb2e613409257
RabidRabbit <aris@bedbugstudio.com>
2015-12-11 16:20:02 +0200

    added methods delete/update

--- sportimo_modules/match-moderation/events-stats-analyzer.js
+++ sportimo_modules/match-moderation/events-stats-analyzer.js
@@ -23,22 +23,47 @@ var parsers = {
 		Add: function (event, match, log) {
 			CurrentMatch = match;
 			StatsMethods.toObject(event);
-				
 			Log = log;
+
 			var evtData = event.data;
 			
 			// Filter based on different sport based event types
 			switch (evtData.type) {
-				case "yellow":
+				default:
 					// Update Match stats (ids, stats to update, names if there are any)
-					StatsMethods.UpsertStat([parsers.e.match_id, parsers.e.team_id, parsers.e.player_id], { yc: 1 }, [null, parsers.e.team_name, parsers.e.player_name]);
+					StatsMethods.UpsertStat([parsers.e.match_id, parsers.e.team_id, parsers.e.player_id], evtData.stats, ["match", parsers.e.team_name, parsers.e.player_name]);
 					break;
 			}
 
 
 		},
-		Update: function (event, match, log) { },
-		Delete: function (event, match, log) { },
+		Update: function (event, match, log) {
+			CurrentMatch = match;
+			StatsMethods.toObject(event);
+			Log = log;
+			
+			var newEvent = event.data;	
+			var previousEvent = _.find(match.timeline[newEvent.state], { id: newEvent.id, match_id: newEvent.match_id });
+			
+			this.Delete({data:previousEvent}, match, log);
+			this.Add(event, match, log);
+        
+		 },
+		Delete: function (event, match, log) {
+			CurrentMatch = match;
+			StatsMethods.toObject(event);
+			Log = log;
+			
+			var evtData = event.data;
+			// Filter based on different sport based event types
+			switch (evtData.type) {
+				default:
+					// Update Match stats (ids, stats to update, names if there are any)
+					StatsMethods.DowndelStat([parsers.e.match_id, parsers.e.team_id, parsers.e.player_id], evtData.stats, ["match", parsers.e.team_name, parsers.e.player_name]);
+					break;
+			}
+			
+		},
 	},
 	basket: {},
 	tennis: {},
@@ -47,42 +72,56 @@ var parsers = {
 
 
 var StatsMethods = {
-	toObject: function(event){
+	toObject: function (event) {
 		parsers.e.data = event.data;
-		parsers.e.match_id =  event.data.match_id;
-		
+		parsers.e.match_id = event.data.match_id;
+
 
 		parsers.e.team_id = CurrentMatch[event.data.team]._id.toString();
-		
-		
+
+
 		parsers.e.team_name = event.data.team;
 		parsers.e.player_id = event.data.players[0].id;
 		parsers.e.player_name = event.data.players[0].name;
-		
+
 	},
-	UpsertStat: function (IDsToUpdate, statsToChange, names) {
+	UpsertStat: function (ids, stats, names) {
 
 		var idscount = 0;
 		// For each id that needs updating
-		_.forEach(IDsToUpdate, function (id) {
+		_.forEach(ids, function (id) {
 			
 			//First check if stat id exists
 			var statIndex = _.findIndex(CurrentMatch.stats, { id: id });
 
 			if (statIndex > -1) {
 				// if it does, update stat keys
-				StatsMethods.InsertStatIfEmpty(CurrentMatch.stats[statIndex], statsToChange);
+				StatsMethods.InsertStatIfEmpty(CurrentMatch.stats[statIndex], stats);
 				if (names[idscount] != null)
 					StatsMethods.InsertStatIfEmpty(CurrentMatch.stats[statIndex], { name: names[idscount] });
 			} else {
 				// if it doesn't, create first and then update stat keys
 				CurrentMatch.stats.push({ id: id });
-				StatsMethods.InsertStatIfEmpty(_.last(CurrentMatch.stats), statsToChange);
+				StatsMethods.InsertStatIfEmpty(_.last(CurrentMatch.stats), stats);
 				if (names[idscount] != null)
 					StatsMethods.InsertStatIfEmpty(_.last(CurrentMatch.stats), { name: names[idscount] });
 			}
 
 			idscount++;
+		});
+
+	},
+	DowndelStat: function (ids, stats) {
+		// For each id that needs updating
+		_.forEach(ids, function (id) {
+			
+			//First check if stat id exists
+			var statIndex = _.findIndex(CurrentMatch.stats, { id: id });
+
+			if (statIndex > -1) {
+				// if it does, update stat keys
+				StatsMethods.DecreaseStat(CurrentMatch.stats[statIndex], stats);	
+			}
 		});
 
 	},
@@ -101,6 +140,18 @@ var StatsMethods = {
 			}
 		});
 
+	},
+	DecreaseStat: function (statkey, statsToChange) {
+
+		var stats = Object.keys(statsToChange);
+
+		_.forEach(stats, function (stat) {
+			if (_.has(statkey, stat)) {
+				statkey[stat] -= statsToChange[stat];
+				if(statkey[stat]< 0) statkey[stat] = 0;
+			} 
+		});
+
 	}
 }
 
@@ -108,9 +159,9 @@ var StatsAnalyzer = {
 	Parse: function (event, match, log) {
 		return parsers[match.sport][event.type](event, match, log);
 	},
-	UpdateStat: function(id, stat, stats){
-		if(stats) CurrentMatch = stats;
-		StatsMethods.UpsertStat([id],stat,[null]);
+	UpsertStat: function (id, stat, stats) {
+		if (stats) CurrentMatch = stats;
+		StatsMethods.UpsertStat([id], stat, [null]);
 	}
 }
 
