77adce235403b79ca264853eddae92693459b345
RabidRabbit <aris@bedbugstudio.com>
2016-01-31 17:00:42 +0200

    added abilities to activate and finilize

--- sportimo_modules/wildcards/controllers/wildcard.js
+++ sportimo_modules/wildcards/controllers/wildcard.js
@@ -8,56 +8,36 @@ var moment = require('moment');
 var WildcardModel = require('../models/wildcard');
 
 var WildCard = function (CardObject) {
-  
+
     // Instance
     var self = this;
-    
+
+    // The var holding the interval
+    this.CountDownTimmer;
+
+    // The ref to the parent module
+    var WildCardsModule;
+
     // Initialization
-    this.init = function (inMilliseconds) {
-        
+    this.init = function (module) {
+
+        // Set the reference to the module
+        WildCardsModule = module;
+
         // Set card defaults according to dbModel data
-        
         setTimeout(function () {
-            self.activate()
-        }, inMilliseconds | this.defaults.delay_timer);
-        
+            self.activate();
+        }, CardObject.activates_in);
+
         // Save initial state to database.
         self.save();
     };
-    
-    
-    // Activate the card.
-    // If activated is false, checks will ignore this card when an event is received
-    this.activate = function () {
-        this.model.status = 1;
-        this.model.activated = moment.utc();
-        this.startDestroyTimmer = setInterval(function () {
-            self.countDownCard()
-        }, 1000);
-        
-        // Save activated state to database
-        this.save();
-    }
-    
+
     // // Default settings
     this.defaults = {
 
-        // Settings
-        shouldUpdate: true,
-
-        // properties
-        max_cards_pertype: 2,
-
-        // Max card points (min/max)
-        card_points: [[60, 120], [30, 60], [40, 80], [20, 40]],
-
-        // timers
-        delay_timer: 20000, // 20 seconds
-        destroy_timer: 300000, // 5 minutes
-        points_step: 10000 // every 10"
     };
-    
-    
+
     // Store the mongoose model
     this.model = new WildcardModel({
         userid: CardObject.userid,
@@ -65,108 +45,97 @@ var WildCard = function (CardObject) {
         minute: CardObject.minute,
         segment: CardObject.segment,
         duration: CardObject.duration,
+        activates_in: CardObject.activates_in,
         timer: CardObject.timer,
         appear_conditions: CardObject.appear_conditions,
         win_conditions: {
             match: CardObject.win_conditions.match,
             stats: CardObject.win_conditions.stats
         },
-        points: 0,
+        points: CardObject.maxpoints,
+        points_step: (CardObject.maxpoints - CardObject.minpoints) / (CardObject.duration / 1000),
         minpoints: CardObject.minpoints,
         maxpoints: CardObject.maxpoints,
         created: moment.utc(),
         activated: null,
         ended: null,
+        won: null,
         status: 0,
         linked_event: 0
-    })
-   
+    });
+    
+    // Set the Id of the card from the model
     this.id = this.model._id.toString();
 
-//    this.pointIncrement = (this.defaults.card_points[cardtype][1] - this.defaults.card_points[cardtype][0]) / (this.defaults.destroy_timer / this.defaults.points_step);
-//
-//
-//
-//
-//
-    this.countDownCard = function () {
-//        this.defaults.destroy_timer = this.defaults.destroy_timer - 1000;
-//        // console.log(this.defaults.destroy_timer)  ;
-//        // console.log(this.defaults.points_step);
-//        // console.log(this.defaults.destroy_timer % this.defaults.points_step);
-//
-//        // Update the card every points_step
-//        if (this.defaults.destroy_timer % this.defaults.points_step == 0) {
-//
-//            // Assign the change in points
-//            this.attributes.countpoints = this.attributes.countpoints - this.pointIncrement;
-//
-//            // Make sure that points don't became negative by mistake
-//            if (this.attributes.countpoints < 0) this.attributes.countpoints = 0;
-//
-//            this.attributes.points = Math.round(this.attributes.countpoints + this.attributes.minpoints);
-//            // Sync the timer that is used in the client for compatibility
-//            this.attributes.timer = this.defaults.destroy_timer / 1000;
-//
-//            if (this.defaults.destroy_timer <= 0) {
-//                log("[Card " + this.attributes.userid + "_" + this.attributes.cardid + "] [Card Timer Finished]", "info");
-//                this.attributes.activated = 2;
-//            }
-//
-//            // We will have to update the database here to keep the card in sync 
-//            this.save();
-//
-//        }
-//
-//        if (this.defaults.destroy_timer <= 0) {
-//            // first remove the card from the Played Cards arra
-//            Wildcards.Remove(this);
-//            // then clear the interval
-//            clearInterval(this.startDestroyTimmer);
-//
-//            // Time's Up - remove the card from play
-//            this.clear();
-//        }
+    // Activate the card.
+    // If model.status == 0, checks will ignore this card when an event is received
+    this.activate = function () {
+        this.model.status = 1;
+        this.model.activated = moment.utc();
+        this.CountDownTimmer = setInterval(function () {
+            self.countDown()
+        }, 1000);
+
+        // Save activated state to database
+        this.save();
     }
 
+
+    this.countDown = function () {
+
+        // Raise timer by 1"
+        this.model.timer += 1000;
+
+        console.log(this.model.timer);
+
+        // Decrease points
+        this.model.points -= this.model.points_step;
+
+        // Safeguard that points cannot be lower tahn the min points 
+        if (this.model.points < this.model.minpoints) this.model.points = this.model.minpoints;
+
+        if (this.model.timer >= this.model.duration) {
+           console.log("[Card " + this.id + "] [Card Timer Finished]", "info");
+            this.model.status = 2;
+            this.model.ended = moment.utc();
+
+            // Clear the interval
+            clearInterval(this.CountDownTimmer);
+
+            // Save state
+            this.save();
+
+            // Remove the card from the cardsInPlay Array
+            WildCardsModule.remove(self);
+
+            // Release object
+            delete self;
+        } else {
+            // Save state
+            this.save();
+        }
+
+
+    };
+
     this.save = function () {
-      self.model.save();
-    }
-//
-//    this.win = function (event_id, delay) {
-//        var windelay = delay || 0;
-//        self.attributes.correct = 1;
-//        self.attributes.activated = 2;
-//        self.attributes.linked_event = event_id;
-//        var options = {};
-//
-//        if (self.defaults.shouldUpdate) {
-//            setTimeout(function () {
-//                needle
-//                    .post(databaseURL + AwardPointsPHP, self.attributes, options, function (error, response) {
-//                        if (!error && response.statusCode == 200) {
-//                            log("[Win response] " + response.body, "info");
-//                        } else
-//                            log("[error] " + error, "error");
-//                    });
-//            }, windelay)
-//
-//        }
-//    }
-//
-    this.delete = function(){
+        self.model.save();
+    };
+
+    this.win = function () {
+
+    };
+
+    // WARNING: This should not be used.
+    // It is for testing purposes ONLY.
+    this.delete = function () {
         this.model.remove();
         this.clear();
-    }
-    
+    };
+
     this.clear = function () {
-        // then clear the interval
-        clearInterval(this.startDestroyTimmer);
-        // first remove the card from the Played Cards array
-//        Wildcards.Remove(this);
-    }
-//    // Initialize the wildcard
-//    // this.init();
+        clearInterval(this.CountDownTimmer);
+    };
 
 };
 
