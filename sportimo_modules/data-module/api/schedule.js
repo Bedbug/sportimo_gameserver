ae9aab28d06c2f5bfefc4f34fcff3245ece012df
RabidRabbit <aris@bedbugstudio.com>
2016-04-27 15:53:34 +0300

    added time & status to schedule

--- sportimo_modules/data-module/api/schedule.js
+++ sportimo_modules/data-module/api/schedule.js
@@ -20,7 +20,7 @@ api.items = function(req, res) {
         .populate('away_team','name logo')
         .populate('competition','name logo');
         
-    q.select('home_team home_score away_team away_score competition time status');
+    q.select('home_team home_score away_team away_score competition time state start');
 
     q.exec(function(err, items) {
 
@@ -62,7 +62,7 @@ api.itemsSearch = function(req, res) {
         .populate('away_team')
         .populate('competition');
         
-    q.select('home_team home_score away_team away_score competition time status');
+    q.select('home_team home_score away_team away_score competition time state start');
 
     if (req.body.limit != undefined)
         q.limit(req.body.limit);
