e74ae12f4a5e2cea6e276252d165671a624b613f
RabidRabbit <aris@bedbugstudio.com>
2016-05-29 18:11:17 +0300

    added settings to match data

--- sportimo_modules/data-module/api/match.js
+++ sportimo_modules/data-module/api/match.js
@@ -25,7 +25,7 @@ api.item = function (req, res) {
 
     // First we get the match data
     Matches.findById(gameid)
-        .select('home_team away_team home_score away_score time isTimeCounting stats timeline start state')
+        .select('home_team away_team home_score away_score time isTimeCounting stats timeline start settings state')
         .populate('home_team', 'name logo')
         .populate('away_team', 'name logo')
         .exec(function (err, match) {
