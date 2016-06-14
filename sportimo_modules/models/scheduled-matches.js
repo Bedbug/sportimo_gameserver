d5c2d23c439fe59f47379618fbacad1a42e97113
RabidRabbit <aris@bedbugstudio.com>
2016-06-14 10:30:59 +0300

    added headtohead property to match model

--- sportimo_modules/models/scheduled-matches.js
+++ sportimo_modules/models/scheduled-matches.js
@@ -12,6 +12,7 @@ else {
     match_id: String,
     type: String,
     stats: mongoose.Schema.Types.Mixed,
+    headtohead: [string],
     playerscount: Number,
     status: String,
     timeline_event: Boolean,
