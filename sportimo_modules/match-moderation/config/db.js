189cec628ee7b967bbf53ad11dd8bc039c2fb17e
RabidRabbit <aris@bedbugstudio.com>
2016-10-17 15:15:19 +0300

    revert back to restored database

--- sportimo_modules/match-moderation/config/db.js
+++ sportimo_modules/match-moderation/config/db.js
@@ -1,21 +1,21 @@
 'use strict';
 var mongoose = require('mongoose');
 
-// var config = {
-//   "db": "sportimov2",  
-//   "host": "ds021165-a1.mlab.com", //"ds027835.mongolab.com",  
-//   "user": "bedbug",
-//   "pw": "a21th21",
-//   "port": "21165" //"27835"
-// };
-
 var config = {
-  "db": "sportimo_mbc",  
-  "host": "ds059726-a0.mlab.com", //"ds027835.mongolab.com",  
+  "db": "sportimov2",  
+  "host": "ds021165-a1.mlab.com", //"ds027835.mongolab.com",  
   "user": "bedbug",
   "pw": "a21th21",
-  "port": "59726" //"27835"
+  "port": "21165" //"27835"
 };
+
+// var config = {
+//   "db": "sportimo_mbc",  
+//   "host": "ds059726-a0.mlab.com", //"ds027835.mongolab.com",  
+//   "user": "bedbug",
+//   "pw": "a21th21",
+//   "port": "59726" //"27835"
+// };
 
 var port = (config.port.length > 0) ? ":" + config.port : '';
 var login = (config.user.length > 0) ? config.user + ":" + config.pw + "@" : '';
