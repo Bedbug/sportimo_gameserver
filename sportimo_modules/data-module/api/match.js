2e073541d7d124e48369d878eff82e0c1f336eb5
RabidRabbit <aris@bedbugstudio.com>
2017-09-24 13:40:08 +0300

    server time fix

--- sportimo_modules/data-module/api/match.js
+++ sportimo_modules/data-module/api/match.js
@@ -102,7 +102,7 @@ api.item = function (req, res) {
                                         });
                                     }
 
-                                    if(game & game.matchData)
+                                    if(game && game.matchData)
                                     game.matchData.server_time = Date.now();
 
                                     return res.status(200).json(game);
